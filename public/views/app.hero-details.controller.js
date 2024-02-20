f0766743c853778d61c41ed6017a9abf272d3f88
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-02-20 18:48:05 -0300

    feat: redirect unauthorized user when accessing private route

--- public/views/app.hero-details.controller.js
+++ public/views/app.hero-details.controller.js
@@ -4,8 +4,15 @@ angular
     .module("app.hero-detail.controller", ["app.heroes.service"])
     .controller(
         "app.hero-detail.controller",
-        function ($scope, $routeParams, heroesService) {
+        function ( $scope, $routeParams, $window, heroesService, authCheckerService){
+            authCheckerService.isUserAuthorized()
+                .then(({isAuth}) => redirectUnauthorizedUser(isAuth));
+
+            function redirectUnauthorizedUser(isAuth) {
+                if(!isAuth) $window.location.href = "http://localhost:8000/login";
+            }
+
             heroesService.getById($routeParams.id)
-                .then(hero => $scope.hero = hero);
+                .then((hero) => ($scope.hero = hero));
         }
     );
--- public/views/app.heroes.controller.js
+++ public/views/app.heroes.controller.js
@@ -1,8 +1,15 @@
 import "/services/heroes.js";
+import "/services/authChecker.js";
 
 angular
-    .module("app.heroes.controller", ["app.heroes.service"])
-    .controller("app.heroes.controller", function ($scope, heroesService) {
-        heroesService.heroes()
-            .then((heroes) => $scope.heroes = heroes);
-    });
+    .module("app.heroes.controller", [
+        "app.heroes.service",
+        "app.authChecker.service",
+    ])
+    .controller(
+        "app.heroes.controller",
+        function ($scope, heroesService, ) {
+            heroesService.heroes()
+                .then((heroes) => ($scope.heroes = heroes));
+        }
+    );
