ece32bfa274bb421ec900b752ddd5a21a55c7123
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-02-21 11:40:59 -0300

    fix: take ng-app and ng-controller apart from guest layout
    the guest layout was designed at first to only be used at login route. However, it was later decided to reuse the layout at sign-up route as well. Thus, it was necessary to remove all logic related to login client-side logic from the layout. Both ng-app and ng-controller has been removed and put on the login view.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" ng-app="login">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" >
 
 <head>
     <meta charset="utf-8">
@@ -18,11 +18,12 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased" ng-controller="loginController">
+<body class="font-sans text-gray-900 antialiased" >
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
 </body>
+
 </html>
