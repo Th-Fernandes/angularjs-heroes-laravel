72aa0b54c5588b36ae972066be2294d153ed99ce
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2024-02-21 11:04:10 -0300

    feat: add missing inputs to the sign-up form
    The sign-up's form was only designed to receive user's email, password and name. Hence codename, phone number and photo url inputs fields have been added. The next step is to build a client side data validation to let the user know if he is typing its data wrongly.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,11 +2,19 @@
     <form method="POST" action="{{ route('sign-up') }}">
         @csrf
 
+        <h2 class="text-center font-bold text-2xl">Sign up for free!</h2>
         <!-- Name -->
-        <div>
+        <div class="mt-6">
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Codename -->
+        <div class="mt-4">
+            <x-input-label for="codename" :value="__('Codename')" />
+            <x-text-input id="codename" class="block mt-1 w-full" type="text" name="codename" :value="old('codename')" required />
+            <x-input-error :messages="$errors->get('codename')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
@@ -16,12 +24,17 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- Photo -->
+        <div class="mt-4">
+            <x-input-label for="photo" :value="__('Photo')" />
+            <x-text-input id="photo" class="block mt-1 w-full" type="text" name="photo" :value="old('photo')" />
+            <x-input-error :messages="$errors->get('photo')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
@@ -32,6 +45,13 @@
             <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <!-- PhoneNumber -->
+        <div class="mt-4">
+            <x-input-label for="phoneNumber" :value="__('PhoneNumber')" />
+            <x-text-input id="phoneNumber" class="block mt-1 w-full" type="text" name="phoneNumber" />
+            <x-input-error :messages="$errors->get('phoneNumber')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
